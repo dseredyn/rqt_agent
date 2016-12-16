ef239a261cc6634faff1307f5e73ed9d1daa4364
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-12-16 17:21:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+rqt_agent
+=========
+
+TODO
---
+++ src/rqt_agent/system_widget.py
@@ -0,0 +1,341 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2011, Dorian Scholz, TU Darmstadt
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the TU Darmstadt nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import division
+import os
+
+from python_qt_binding import loadUi
+from python_qt_binding.QtCore import Qt, QTimer, Signal, Slot
+from python_qt_binding.QtWidgets import QHeaderView, QMenu, QTreeWidgetItem, QWidget, QVBoxLayout, QLabel
+#from PyQt5 import QtCore, QtGui, QtWidgets
+import roslib
+import rospkg
+import rospy
+from rospy.exceptions import ROSException
+
+#from .topic_info import TopicInfo
+from rqt_topic.topic_info import TopicInfo
+from .subsystem_widget import SubsystemWidget
+
+class SystemWidget(QWidget):
+    """
+    main class inherits from the ui window class.
+
+    You can specify the topics that the topic pane.
+
+    SystemWidget.start must be called in order to update topic pane.
+    """
+
+    SELECT_BY_NAME = 0
+    SELECT_BY_MSGTYPE = 1
+
+    _column_names = ['topic', 'type', 'bandwidth', 'rate', 'value']
+
+    def __init__(self, plugin=None, selected_topics=None, select_topic_type=SELECT_BY_NAME):
+        """
+        @type selected_topics: list of tuples.
+        @param selected_topics: [($NAME_TOPIC$, $TYPE_TOPIC$), ...]
+        @type select_topic_type: int
+        @param select_topic_type: Can specify either the name of topics or by
+                                  the type of topic, to filter the topics to
+                                  show. If 'select_topic_type' argument is
+                                  None, this arg shouldn't be meaningful.
+        """
+        super(SystemWidget, self).__init__()
+
+#        levels_layout = QVBoxLayout()
+        nameLabel = QLabel("Name:")
+#        levels_layout.addWidget(nameLabel)
+
+#        self._select_topic_type = select_topic_type
+
+        rp = rospkg.RosPack()
+        ui_file = os.path.join(rp.get_path('rqt_agent'), 'resource', 'SystemWidget.ui')
+        loadUi(ui_file, self)
+
+#        self.verticalLayout.addWidget(nameLabel)
+
+        self._plugin = plugin
+#        self.topics_tree_widget.sortByColumn(0, Qt.AscendingOrder)
+#        header = self.topics_tree_widget.header()
+#        header.setResizeMode(QHeaderView.ResizeToContents)
+#        header.customContextMenuRequested.connect(self.handle_header_view_customContextMenuRequested)
+#        header.setContextMenuPolicy(Qt.CustomContextMenu)
+
+        # Whether to get all topics or only the topics that are set in advance.
+        # Can be also set by the setter method "set_selected_topics".
+#        self._selected_topics = selected_topics
+
+#        self._current_topic_list = []
+        self._subsystems = {}
+
+        self._widgets = {}
+
+        self.structure_root = None
+        self.structure_graph = None
+
+#        self._tree_items = {}
+#        self._column_index = {}
+#        for column_name in self._column_names:
+#            self._column_index[column_name] = len(self._column_index)
+
+        # self.refresh_topics()
+
+        # init and start update timer
+        self._timer_refresh_topics = QTimer(self)
+        self._timer_refresh_topics.timeout.connect(self.refresh_topics)
+
+    def start(self):
+        """
+        This method needs to be called to start updating topic pane.
+        """
+        self._timer_refresh_topics.start(1000)
+
+    def generateStructure(self):
+        s_g = {}
+        s_g_parents = {}
+
+        names = []
+
+        for w1_name in self._widgets:
+            names.append(w1_name)
+            w1 = self._widgets[w1_name]
+            for w2_name in self._widgets:
+                w2 = self._widgets[w2_name]
+                rel_pose = w1.getOtherSubsystemRelativePose(w2)
+                if rel_pose != None and rel_pose[0] == 'below':
+                    if not w1_name in s_g:
+                        s_g[w1_name] = {}
+                    s_g[w1_name][rel_pose[1]] = w2_name
+                    s_g_parents[w2_name] = w1_name
+
+        # get top-most subsystem
+        root = None
+        if s_g_parents:
+            root = list(s_g_parents.keys())[0]
+            while root in s_g_parents:
+                root = s_g_parents[root]
+
+        if root == None:
+            return (None, names)
+
+        return (root, s_g)
+
+    @Slot()
+    def refresh_topics(self):
+        """
+        refresh tree view items
+        """
+
+        #
+        # update the list of subsystems
+        #
+        topic_list = rospy.get_published_topics()
+        if topic_list is None:
+            rospy.logerr('Not even a single published topic found. Check network configuration')
+            return
+
+        # start new topic dict
+        new_subsystems = {}
+
+        for topic_name, topic_type in topic_list:
+            name_split = topic_name.split('/')
+#            print name_split
+
+            if (len(name_split) == 3) and (name_split[0] == '') and (name_split[2] == 'diag') and (topic_type == "diagnostic_msgs/DiagnosticArray"):
+                subsystem_name = name_split[1]
+                # if topic is new
+                if subsystem_name not in self._subsystems:
+                    # create new TopicInfo
+                    topic_info = TopicInfo(topic_name, topic_type)
+                    new_subsystems[subsystem_name] = topic_info
+                    topic_info.start_monitoring()
+                else:
+                    # if topic has been seen before, copy it to new dict and
+                    # remove it from the old one
+                    new_subsystems[subsystem_name] = self._subsystems[subsystem_name]
+                    del self._subsystems[subsystem_name]
+
+        # switch to new topic dict
+        self._subsystems = new_subsystems
+
+        #
+        # update each subsystem
+        #
+        new_widgets = {}
+        for subsystem_name in self._subsystems:
+            msg = self._subsystems[subsystem_name].last_message
+
+            if (msg != None) and (len(msg.status) == 2) and \
+              msg.status[0].name == 'components' and msg.status[1].name == 'diagnostics':
+                print "aa"
+                name_split = subsystem_name.split('/')
+
+                if not subsystem_name in self._widgets:
+                    new_widgets[subsystem_name] = SubsystemWidget(self._plugin, subsystem_name)
+                    self.verticalLayout.addWidget(new_widgets[subsystem_name])
+                else:
+                    new_widgets[subsystem_name] = self._widgets[subsystem_name]
+                    del self._widgets[subsystem_name]
+
+        self._widgets = new_widgets
+
+        self.generateStructure()
+
+        #TODO: compare new structure with previous one and update the view (dictionary comparison)
+
+        structure_change = False    # TODO
+
+        if structure_change:
+            # remove all widgets from layout
+            for widget_name in self._widgets:
+                self.verticalLayout.removeWidget(self._widgets[widget_name])
+
+            # add all widgets to layout
+            for widget_name in self._widgets:
+                self.verticalLayout.addWidget(self._widgets[widget_name])
+
+        for widget_name in self._widgets:
+            self._widgets[widget_name].update_subsystem()
+
+
+    def update_value(self, topic_name, message):
+        if hasattr(message, '__slots__') and hasattr(message, '_slot_types'):
+            for slot_name in message.__slots__:
+                self.update_value(topic_name + '/' + slot_name, getattr(message, slot_name))
+
+        elif type(message) in (list, tuple) and (len(message) > 0) and hasattr(message[0], '__slots__'):
+
+            for index, slot in enumerate(message):
+                if topic_name + '[%d]' % index in self._tree_items:
+                    self.update_value(topic_name + '[%d]' % index, slot)
+                else:
+                    base_type_str, _ = self._extract_array_info(self._tree_items[topic_name].text(self._column_index['type']))
+                    self._recursive_create_widget_items(self._tree_items[topic_name], topic_name + '[%d]' % index, base_type_str, slot)
+            # remove obsolete children
+            if len(message) < self._tree_items[topic_name].childCount():
+                for i in range(len(message), self._tree_items[topic_name].childCount()):
+                    item_topic_name = topic_name + '[%d]' % i
+                    self._recursive_delete_widget_items(self._tree_items[item_topic_name])
+        else:
+            if topic_name in self._tree_items:
+                self._tree_items[topic_name].setText(self._column_index['value'], repr(message))
+
+    def _extract_array_info(self, type_str):
+        array_size = None
+        if '[' in type_str and type_str[-1] == ']':
+            type_str, array_size_str = type_str.split('[', 1)
+            array_size_str = array_size_str[:-1]
+            if len(array_size_str) > 0:
+                array_size = int(array_size_str)
+            else:
+                array_size = 0
+
+        return type_str, array_size
+
+    @Slot('QPoint')
+    def handle_header_view_customContextMenuRequested(self, pos):
+        header = self.topics_tree_widget.header()
+
+        # show context menu
+        menu = QMenu(self)
+        action_toggle_auto_resize = menu.addAction('Toggle Auto-Resize')
+        action = menu.exec_(header.mapToGlobal(pos))
+
+        # evaluate user action
+        if action is action_toggle_auto_resize:
+            if header.resizeMode(0) == QHeaderView.ResizeToContents:
+                header.setResizeMode(QHeaderView.Interactive)
+            else:
+                header.setResizeMode(QHeaderView.ResizeToContents)
+
+    @Slot('QPoint')
+    def on_topics_tree_widget_customContextMenuRequested(self, pos):
+        item = self.topics_tree_widget.itemAt(pos)
+        if item is None:
+            return
+
+        # show context menu
+        menu = QMenu(self)
+        action_item_expand = menu.addAction(QIcon.fromTheme('zoom-in'), 'Expand All Children')
+        action_item_collapse = menu.addAction(QIcon.fromTheme('zoom-out'), 'Collapse All Children')
+        action = menu.exec_(self.topics_tree_widget.mapToGlobal(pos))
+
+        # evaluate user action
+        if action in (action_item_expand, action_item_collapse):
+            expanded = (action is action_item_expand)
+
+            def recursive_set_expanded(item):
+                item.setExpanded(expanded)
+                for index in range(item.childCount()):
+                    recursive_set_expanded(item.child(index))
+            recursive_set_expanded(item)
+
+    def shutdown_plugin(self):
+        for topic in self._topics.values():
+            topic['info'].stop_monitoring()
+        self._timer_refresh_topics.stop()
+
+    def set_selected_topics(self, selected_topics):
+        """
+        @param selected_topics: list of tuple. [(topic_name, topic_type)]
+        @type selected_topics: []
+        """
+        rospy.logdebug('set_selected_topics topics={}'.format(
+                                                         len(selected_topics)))
+        self._selected_topics = selected_topics
+
+    # TODO(Enhancement) Save/Restore tree expansion state
+    def save_settings(self, plugin_settings, instance_settings):
+        header_state = self.topics_tree_widget.header().saveState()
+        instance_settings.set_value('tree_widget_header_state', header_state)
+
+    def restore_settings(self, pluggin_settings, instance_settings):
+        if instance_settings.contains('tree_widget_header_state'):
+            header_state = instance_settings.value('tree_widget_header_state')
+            if not self.topics_tree_widget.header().restoreState(header_state):
+                rospy.logwarn("rqt_topic: Failed to restore header state.")
+
+class TreeWidgetItem(QTreeWidgetItem):
+
+    def __init__(self, check_state_changed_callback, topic_name, parent=None):
+        super(TreeWidgetItem, self).__init__(parent)
+        self._check_state_changed_callback = check_state_changed_callback
+        self._topic_name = topic_name
+        self.setCheckState(0, Qt.Unchecked)
+
+    def setData(self, column, role, value):
+        if role == Qt.CheckStateRole:
+            state = self.checkState(column)
+        super(TreeWidgetItem, self).setData(column, role, value)
+        if role == Qt.CheckStateRole and state != self.checkState(column):
+            self._check_state_changed_callback(self._topic_name)
